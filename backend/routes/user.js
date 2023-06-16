fa79b90482d4765a391be83be56b5b8c74603884
ANUSHKA <medirattaanushka23@gmail.com>
2023-06-17 01:16:51 +0530

    User Functionality

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,20 @@
-const express=require('express')
-const requireLogin = require('../middleware/requireLogin')
-const User = require('../models/user')
-const router=express.Router()
+const express = require('express');
+const requireLogin = require('../middleware/requireLogin');
+const User = require('../models/user');
+const Post = require('../models/post');
+const router = express.Router();
 
-router.get("/user/:id",requireLogin,(req,res)=>{
-    User
+router.get("/user/:id", requireLogin, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id }).select("-password");
+    const posts = await Post.find({ postedBy: req.params.id })
+      .populate("postedBy", "_id name")
+      .exec();
 
-})
+    return res.status(200).json({ user, posts });
+  } catch (err) {
+    return res.status(422).json({ error: err });
+  }
+});
 
-module.exports=router
+module.exports = router;

fd7aace1f555121945f6f344f2d5a438eec17042
ANUSHKA <medirattaanushka23@gmail.com>
2023-06-17 01:00:47 +0530

    final check

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,11 @@
 const express=require('express')
+const requireLogin = require('../middleware/requireLogin')
+const User = require('../models/user')
 const router=express.Router()
 
+router.get("/user/:id",requireLogin,(req,res)=>{
+    User
+
+})
 
 module.exports=router
